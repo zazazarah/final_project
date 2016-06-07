4ba45459b81f420043d9cb7771e2dc0ec6ad3caf
zazazarah <mllezarah@gmail.com>
2016-06-07 16:30:37 -0700

    Add files via upload

---
+++ smacdown/js/smacdown.js
@@ -0,0 +1,18 @@
+$(document).ready(function(event) {
+	event.preventDefault();
+
+// $( "#dialog_trigger" ).click(function() {
+// $( "#dialog" ).dialog( "open" );
+// });
+// $("#dialog").dialog({
+//     autoOpen: false,
+//     position: 'center' ,
+//     title: '',
+//     draggable: false,
+//     width : 800,
+//     height : 700, 
+//     resizable : true,
+//     modal : true,
+	
+
+});
